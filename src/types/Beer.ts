0eff627a0ddb3f3b2ddf54534c1830834f8a77a4
Nick <n.m.phelan@gmail.com>
2023-08-20 09:01:58 +0100

    Added types for beer obj

---
+++ src/types/Beer.ts
@@ -0,0 +1,64 @@
+export type Beer = {
+  id: number;
+  name: string;
+  tagline: string;
+  first_brewed: string;
+  description: string;
+  image: string;
+  abv: number;
+  ibu: number;
+  target_fg: number;
+  target_og: number;
+  ebc: number;
+  srm: number;
+  ph: number;
+  attenuation_level: number;
+  volume: Volume;
+  boil_volume: BoilVolume;
+  method: Method;
+  ingredients: Ingredients;
+  food_pairing: string[];
+  brewers_tips: string;
+  contributed_by: string;
+};
+
+type Ingredients = {
+  malt: Malt[];
+  hops: Hops[];
+  yeast: string;
+};
+
+type Malt = {
+  name: string;
+  amount: Volume;
+};
+
+type Hops = {
+  name: string;
+  amount: Volume;
+};
+
+type Method = {
+  mash_temp: MashTemp[];
+  fermentation: Fermentation;
+  twist?: unknown;
+};
+
+type Fermentation = {
+  temp: Volume;
+};
+
+type MashTemp = {
+  temp: Volume;
+  duration: number;
+};
+
+type Volume = {
+  value: number;
+  unit: string;
+};
+
+type BoilVolume = {
+  value: number;
+  unit: string;
+};
